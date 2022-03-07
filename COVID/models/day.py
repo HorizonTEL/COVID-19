2b20ac80ec75e9b4ced41686e195c1e5ea9d7b3f
Horizon <74846298+JZY-nuist@users.noreply.github.com>
2022-03-07 22:34:44 +0800

    Update day.py

--- COVID/models/day.py
+++ COVID/models/day.py
@@ -13,6 +13,29 @@ class Day(db.Model):
     day_cured = db.Column(db.Integer)
     day_died = db.Column(db.Integer)
 
+    
+def initial_day():
+    days = [
+        Day(day_time='', day_confirm=3757, day_died=12, day_cured=312),
+        Day(day_time='', day_confirm=6816, day_died=36, day_cured=345),
+        Day(day_time='', day_confirm=6707, day_died=89, day_cured=327),
+        Day(day_time='', day_confirm=2818, day_died=40, day_cured=325),
+        Day(day_time='', day_confirm=4227, day_died=73, day_cured=362),
+        Day(day_time='', day_confirm=5347, day_died=80, day_cured=376),
+        Day(day_time='', day_confirm=5386, day_died=254, day_cured=597),
+        Day(day_time='', day_confirm=14376, day_died=326, day_cured=624),
+        Day(day_time='', day_confirm=38612, day_died=424, day_cured=667),
+        Day(day_time='', day_confirm=40351, day_died=376, day_cured=710),
+        Day(day_time='', day_confirm=20092, day_died=198, day_cured=648),
+        Day(day_time='', day_confirm=49768, day_died=408, day_cured=614),
+        Day(day_time='', day_confirm=23642, day_died=341, day_cured=712),
+        Day(day_time='', day_confirm=19272, day_died=233, day_cured=806),
+        Day(day_time='', day_confirm=25072, day_died=513, day_cured=806)
+    ]
+    
+    db.session.add_all(days)
+    db.session.commit()
+
 
 def get_day_data():
     headers = {
